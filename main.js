1d8dafba4112e39b7a2df524f3d99312a4fc3556
Luis Aguirre <aguirre.lr@obmontessori.edu.ph>
2026-02-20 11:26:15 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//Function for Discount Button
+function buttonDiscount (){
+    let origBudget = document.getElementById(budgetInput).value;
+    //const discountValue = .2;
+    let discountedPrice = Number(origBudget) * 0.2;
+}
